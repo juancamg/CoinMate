b1f294ddf57632191234d4d5ea0e46257768cfb6
Juan Carlos <jmagar903@g.educaand.es>
2024-02-25 19:33:47 +0100

    AppBar y BottomBar hecho.
    TODO: Quitar los botones de navegacion. Poner los titulos en el AppBar. Mejorar funcionamiento de BottomBar.

--- app/src/main/java/com/example/proyecto_coinmate/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_coinmate/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.proyecto_coinmate
 import android.content.Context
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
